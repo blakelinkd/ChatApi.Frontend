718b47aabd616634b92b860292e2dc246bc7e1d9
Blake Link <blakelinkd@outlook.com>
2024-05-15 19:06:24 -0500

    env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,9 @@ export const environment = {
     getEndpoint: 'https://fondly.ai/api/messages/',
     postEndpoint: 'https://fondly.ai/api/messages/',
   };
+
+  // export const environment = {
+  //   production: false,
+  //   getEndpoint: 'http://localhost:8000/api/messages/',
+  //   postEndpoint: 'http://localhost:8000/api/messages/',
+  // };
