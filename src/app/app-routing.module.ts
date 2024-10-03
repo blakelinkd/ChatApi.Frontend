6f754400cee07a038ece381563424e514442247d
Blake Link <blakelinkd@outlook.com>
2024-10-03 15:52:59 -0500

    Refactor angular.json to include new assets folder for redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RedirectComponent } from './redirect/redirect.component'; 
 const routes: Routes = [
-  { path: 'redirect/github', component: RedirectComponent, data: { url: 'https://github.com/blakelinkd' }},
-  { path: 'redirect/linkedin', component: RedirectComponent, data: { url: 'https://linkedin.com/in/blakelinkd' }},
-  { path: '**', redirectTo: '', pathMatch: 'full' }
-  // Other routes
 ];
 
 @NgModule({
